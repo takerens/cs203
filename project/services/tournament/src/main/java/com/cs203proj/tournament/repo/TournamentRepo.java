14caba4efbaeef687715dba49688c5675c280906
takerens <itsamemario2002@gmail.com>
2024-09-18 16:17:16 +0800

    removing of files
    removing redundant project files

--- project/services/tournament/src/main/java/com/cs203proj/tournament/repo/TournamentRepo.java
+++
@@ -1,9 +0,0 @@
-package services.tournament.src.main.java.com.cs203proj.tournament.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import services.tournament.src.main.java.com.cs203proj.tournament.model.Tournament;
-
-@Repository
-public interface TournamentRepo extends JpaRepository<Tournament, Long> {
-}
