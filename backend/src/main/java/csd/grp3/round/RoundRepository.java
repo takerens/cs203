5d497b2b66ed794985e673014c77786871199764
Quek Shin Leng <sl.quek.2023@scis.smu.edu.sg>
2024-09-27 21:11:02 +0800

    Removed backend file

--- backend/src/main/java/csd/grp3/round/RoundRepository.java
+++
@@ -1,7 +0,0 @@
-// package csd.grp3.round;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface RoundRepository extends JpaRepository<Round, Long>{
-//     // Optional<Match> findByPlayer
-// }   
