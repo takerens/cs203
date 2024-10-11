6bafd2556f0961e0cd778d4f831849f5641057a5
takerens <itsamemario2002@gmail.com>
2024-10-11 19:44:12 +0800

    listTournaments
    listTournaments_NoTournaments_ReturnEmptyList() complete

--- src/test/java/csd/grp3/Testing/TournamentServiceImplTest.java
+++ src/test/java/csd/grp3/Testing/TournamentServiceImplTest.java
@@ -1,25 +1,31 @@
 package csd.grp3.Testing;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import csd.grp3.tournament.Tournament;
 import csd.grp3.tournament.TournamentRepository;
 import csd.grp3.tournament.TournamentServiceImpl;
-import csd.grp3.user.User;
+// import csd.grp3.user.User;
 
 @ExtendWith(MockitoExtension.class)
 public class TournamentServiceImplTest {
@@ -30,12 +36,12 @@ public class TournamentServiceImplTest {
     @InjectMocks
     private TournamentServiceImpl tournamentService;
 
-    // private Tournament tournament;
+    private Tournament tournament;
     // private User player;
 
-    // @BeforeEach
-    // void setUp() {
-    //     MockitoAnnotations.openMocks(this);
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     //     tournament = new Tournament();
     //     tournament.setId(1L);
     //     tournament.setTitle("Test Tournament");
@@ -43,13 +49,14 @@ public class TournamentServiceImplTest {
         
     //     player = new User("testUser", "testPassword123");  // Username and password
     //     player.setAuthorities("ROLE_PLAYER"); // Set specific authorities
-    // }
+    }
 
     @Test
     void listTournaments_NoTournaments_ReturnEmptyList() {
+        List<Tournament> tournamentsList = new ArrayList<>();
+
         // mock the getAllTournaments()
-        when(tournamentRepository.getAllTournaments())
-        .thenReturn(new ArrayList<>());
+        when(tournamentRepository.getAllTournaments()).thenReturn(tournamentsList);
 
         List<Tournament> result = tournamentService.listTournaments();
 
@@ -59,18 +66,49 @@ public class TournamentServiceImplTest {
 
     @Test
     void listTournaments_HasTournaments_ReturnListOfTournaments() {
+        // Arrange
         Tournament tournament = new Tournament();
         List<Tournament> tournamentsList = new ArrayList<>();
         tournamentsList.add(tournament);
         
         // mock findAll()
         when(tournamentRepository.findAll()).thenReturn(tournamentsList);
+        // mock adding
 
         List<Tournament> result = tournamentService.listTournaments();
 
         assertEquals(1, result.size());
         assertEquals("Test Tournament", result.get(0).getTitle());
         verify(tournamentRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getTournament_NoTournament_ReturnNull() {
+        // mock findById
+        when(tournamentRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        // Act
+        Tournament tournament = tournamentService.getTournament(0L);
+
+        // Assert
+        assertNull(tournament);
+        verify(tournamentRepository).findById(0L);
+    }
+
+    @Test
+    void getTournament_TournamentFound_ReturnTournament() {
+        // Mock findById to return the tournament
+        when(tournamentRepository.findById(1L))
+        .thenReturn(Optional.of(tournament));
+
+        // Act
+        Tournament foundTournament = tournamentService.getTournament(1L);
+
+        // Assert
+        assertNotNull(foundTournament);
+        assertEquals(1L, foundTournament.getId());
+        assertEquals("Test Tournament", foundTournament.getTitle());
+        verify(tournamentRepository).findById(1L);
     }
 
     @Test
