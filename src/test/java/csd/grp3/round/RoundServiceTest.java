c75c4351544484a7f64bca8f68d7b13e67d0bb7b
Daniel <ggsan.com@gmail.com>
2024-10-16 18:32:34 +0800

    modified round testing

--- src/test/java/csd/grp3/round/RoundServiceTest.java
+++ src/test/java/csd/grp3/round/RoundServiceTest.java
@@ -1,9 +1,12 @@
 package csd.grp3.round;
 
+import csd.grp3.match.Match;
 import csd.grp3.tournament.Tournament;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class RoundServiceTest {
@@ -73,5 +77,39 @@ public class RoundServiceTest {
             roundService.getRound(invalid_round.getId());
         });
         verify(rounds).findById(invalid_round.getId());
+    }
+
+    @Test
+    void isOver_allMatchOver() {
+        // arrange
+        Round round = new Round();
+        Match match1 = new Match();
+        match1.setResult(1);
+        Match match2 = new Match();
+        match2.setResult(0.5);
+        Match match3 = new Match();
+        match3.setResult(-1);
+        Match match4 = new Match();
+        match4.setResult(1);
+        round.getMatches().addAll(List.of(match1, match2, match3, match4));
+
+        assertTrue(round.isOver());
+    }
+
+    @Test
+    void isOver_notAllMatchOver() {
+        // arrange
+        Round round = new Round();
+        Match match1 = new Match();
+        match1.setResult(1);
+        Match match2 = new Match();
+        match2.setResult(0.5);
+        Match match3 = new Match();
+        match3.setResult(0);
+        Match match4 = new Match();
+        match4.setResult(1);
+        round.getMatches().addAll(List.of(match1, match2, match3, match4));
+
+        assertFalse(round.isOver());
     }
 }
