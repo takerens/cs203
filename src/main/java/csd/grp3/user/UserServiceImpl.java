25f7c9b2be0eeb2caba149434b386f74395509ee
Jing Wei <jingwei.teh.2023@scis.smu.edu.sg>
2024-11-08 18:48:19 +0800

    edited jwt token to also return authorities, added comments

--- src/main/java/csd/grp3/user/UserServiceImpl.java
+++ src/main/java/csd/grp3/user/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -86,7 +87,7 @@ public class UserServiceImpl implements UserService {
 
         //Return the user if the password matches
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(username);
+            return jwtService.generateToken((UserDetails) authentication.getPrincipal());
         }
         //Else throw exception
         throw new BadCredentialsException("Username and Password do not match");
