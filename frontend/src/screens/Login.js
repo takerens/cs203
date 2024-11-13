b473a9020ab5c64ea93df97b80dcc6da9d6c1688
Quek Shin Leng <sl.quek.2023@scis.smu.edu.sg>
2024-11-13 12:41:14 +0800

    Reapply "Add redirect for logged-in users on login page"
    This reverts commit de164dd12dd9ae82d8beb313f5d3a87a76753473.

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
 import UserForm from '../components/user/UserForm';
@@ -7,6 +7,12 @@ import { handleLogin } from '../utils/UserUtils';
 const Login = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => { // When login page, check if user is already logged in then redirect to home page
+    if (localStorage.getItem('jwtToken')) {
+      navigate('/tournaments');
+    }
+}, []);
 
   const handleSubmit = async (userData) => {
     await handleLogin(userData, setErrorMessage, navigate);
